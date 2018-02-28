16086d58f3e1ee863a5f96bd763a4bbdf20eabfc
Óscar Ferrando <oferrando@biit-solutions.com>
2018-02-28 18:56:51 +0100

    first commit on tables representation

--- form-runner-demo/src/main/java/com/biit/demo/DemoWebformRunnerTable.java
+++ form-runner-demo/src/main/java/com/biit/demo/DemoWebformRunnerTable.java
@@ -6,21 +6,17 @@ import com.biit.formrunner.common.IRunnerElement;
 import com.biit.formrunner.common.Runner;
 import com.biit.formrunner.webforms.IWebformsRunnerGroup;
 import com.biit.formrunner.webforms.WebformsRunnerElementFactory;
-import com.biit.formrunner.webforms.WebformsRunnerGroup;
-import com.biit.webforms.persistence.entity.Group;
+import com.biit.formrunner.webforms.WebformsRunnerTable;
 
-public class DemoWebformRunnerGroup extends WebformsRunnerGroup implements IWebformsRunnerGroup {
+public class DemoWebformRunnerTable extends WebformsRunnerTable implements IWebformsRunnerGroup {
 	private static final long serialVersionUID = -3485645350589558706L;
 
-	public DemoWebformRunnerGroup(BaseGroup group, Runner runner) {
+	public DemoWebformRunnerTable(BaseGroup group, Runner runner) {
 		super(group, runner);
 	}
 
 	@Override
 	public IWebformsRunnerGroup getElement(BaseGroup group, Runner runner) {
-		if(((Group) group).isShownAsTable()) {
-			return new DemoWebformRunnerTable(group, runner);
-		}
 		return new DemoWebformRunnerGroup(group, runner);
 	}
 
