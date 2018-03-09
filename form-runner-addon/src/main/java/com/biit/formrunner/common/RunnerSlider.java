371e63ce76cb7d92643b453f2368f9e1a6b9c845
Jorge Hortelano <jorgehortelano@gmail.com>
2018-03-09 15:23:19 +0100

    Fixed Slider causes and exception when retrieving stored data
     Unable to convert value of type java.lang.String to presentation type
    class java.lang.Double.

--- form-runner-addon/src/main/java/com/biit/formrunner/common/RunnerSlider.java
+++ form-runner-addon/src/main/java/com/biit/formrunner/common/RunnerSlider.java
@@ -2,14 +2,15 @@ package com.biit.formrunner.common;
 
 import java.util.List;
 
+import com.biit.form.runner.logger.FormRunnerLogger;
 import com.vaadin.ui.Slider;
 import com.vaadin.ui.Slider.ValueOutOfBoundsException;
 
 public class RunnerSlider extends RunnerField<Slider> {
 	private static final long serialVersionUID = 5508500902295259321L;
 
-	public RunnerSlider(String name, String caption, double minValue, double maxValue, String description,
-			String requiredCaption, boolean isMandatory, Double defaultValue, Runner runner, List<String> path) {
+	public RunnerSlider(String name, String caption, double minValue, double maxValue, String description, String requiredCaption, boolean isMandatory,
+			Double defaultValue, Runner runner, List<String> path) {
 		super(name, new Slider(caption), description, isMandatory, requiredCaption, runner, path);
 		getComponent().setMin(minValue);
 		getComponent().setMax(maxValue);
@@ -24,5 +25,16 @@ public class RunnerSlider extends RunnerField<Slider> {
 	@Override
 	public void clear() {
 		getComponent().getMin();
+	}
+
+	@Override
+	public void setAnswers(List<String> answers) throws UnsupportedOperationException {
+		if (answers != null && !answers.isEmpty()) {
+			try {
+				getComponent().setValue(Double.parseDouble(answers.get(0)));
+			} catch (NumberFormatException nfe) {
+				FormRunnerLogger.errorMessage(this.getClass().getName(), "Invalid value '" + answers.get(0) + "' retrieved for slider '" + getName() + "'.");
+			}
+		}
 	}
 }
